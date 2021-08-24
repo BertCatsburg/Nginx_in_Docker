7aab472fce39b0f76ceac31575b5dd3496c3c567
bert@catsburg.com <bert@catsburg.com>
2021-08-24 13:57:32 +0200

    First Commit

---
+++ deployment/conf/nginx_helpers.js
@@ -0,0 +1,15 @@
+function aport(r) {
+    return process.env.PORTA;
+}
+
+function bport(r) {
+    return process.env.PORTB;
+}
+
+function hostipa(r) {
+    return process.env.HOSTIPA;
+}
+
+function hostipb(r) {
+    return process.env.HOSTIPB;
+}
